82bc7fdef963b536a979dc3a44ceaa7d96492f8e
xayrullaevziyovuddin <xayrullaevziyovuddin@gmail.com>
2024-11-01 17:36:29 +0500

    dubug

--- todos_app/urls.py
+++ todos_app/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TodoViewSet
 
 router = DefaultRouter()
-router.register(r'todos', TodoViewSet)
+router.register(r'', TodoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
