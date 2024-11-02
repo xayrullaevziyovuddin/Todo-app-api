dd3861c3ccfe8c13bf54bac10f9eacb42332ab9e
Muxtorxon <chifuyu0711@gmail.com>
2024-11-02 17:02:04 +0500

    test

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,23 +1,8 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
 
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (AllowAny,)
+class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        refresh = RefreshToken.for_user(user)
-        return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }, status=status.HTTP_201_CREATED)
