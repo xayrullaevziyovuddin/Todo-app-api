095c2c3210b42eb14308124cea8b093f5a5c3293
xayrullaevziyovuddin <xayrullaevziyovuddin@gmail.com>
2024-11-16 12:01:42 +0500

    deploy

--- todos_app/serializers.py
+++ todos_app/serializers.py
@@ -2,14 +2,13 @@ from rest_framework import serializers
 from .models import Todo
 
 
-def validate_title(value):
-    if Todo.objects.filter(title=value).exists():
-        raise serializers.ValidationError("Задача с таким заголовком уже существует.")
-    return value
-
-
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ['id', 'title', 'status', 'due_date', 'created_date', 'is_special_day']
         read_only_fields = ['user']  # Делаем поле "user" только для чтения
+
+    def validate_title(self, value):
+        if Todo.objects.filter(title=value, user=self.context['request'].user).exists():
+            raise serializers.ValidationError("Задача с таким заголовком уже существует.")
+        return value
